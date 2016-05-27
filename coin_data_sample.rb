0f57aba34052eb85a0773ec057b3be378ebbf026
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2016-05-27 11:34:50 -0600

    Update sha256 in coin_data_sample.rb

--- coin_data_sample.rb
+++ coin_data_sample.rb
@@ -2,6 +2,7 @@ class CoinDataSample < Formula
   desc "Sample models"
   homepage "http://www.coin-or.org/download/pkgsource/Data"
   url "http://www.coin-or.org/download/pkgsource/Data/Data-Sample-1.2.10.tgz"
+  sha256 "ec7de931a06126040910964b6ce89a3d0cf64132fdde187689cc13277e2c1985"
 
   def install
     system "./configure", "--disable-debug",
