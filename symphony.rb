b68785e8c3eac9a2d59826c6f426b0ba961b15c7
Ted Ralphs <ted@lehigh.edu>
2016-06-09 13:22:22 -0400

    Using standard version of Glpk and passing options through to dependencies properly

--- symphony.rb
+++ symphony.rb
@@ -1,22 +1,29 @@
 class Symphony < Formula
   desc "Framework for solving mixed integer linear programs"
   homepage "https://projects.coin-or.org/SYMPHONY"
-  url "http://www.coin-or.org/download/pkgsource/SYMPHONY/SYMPHONY-5.6.13.tgz"
-  sha256 "62353422dae24c24fe02b41ba9ffeecbcda2940b68cf477c2867df0aebaa6812"
+  url "http://www.coin-or.org/download/pkgsource/SYMPHONY/SYMPHONY-5.6.14.tgz"
+  sha256 "666d1c72c4bb084b470c4f066b1694f7b3b11a479de15d9d1e9bfe1b53e3ae63"
   head "https://projects.coin-or.org/svn/SYMPHONY/trunk"
 
   option "without-openmp", "Disable openmp support"
-  option "with-gmpl", "GNU Modeling Language support via GLPK"
-
-  depends_on "homebrew/science/asl" => :optional
-  depends_on "homebrew/science/glpk" => :optional
-  depends_on "homebrew/science/openblas" => :optional
+  option "with-asl", "Build CLP with ASL support"
+  option "with-glpk", "Build with support for reading AMPL/GMPL models"
+  option "with-mumps", "Build CLP with MUMPS support"
+  option "with-openblas", "Build CLP with OpenBLAS support"
+  option "with-suite-sparse", "Build CLP with SuiteSparse support"
 
   asl_dep = (build.with? "asl") ? ["with-asl"] : []
+  glpk_dep = (build.with? "glpk") ? ["with-glpk"] : []
+  mumps_dep = (build.with? "mumps") ? ["with-mumps"] : []
   openblas_dep = (build.with? "openblas") ? ["with-openblas"] : []
-
+  suitesparse_dep = (build.with? "suite-sparse") ? ["with-suite-sparse"] : []
+  
+  depends_on "homebrew/science/openblas" => :optional
+  depends_on "homebrew/science/glpk448" if build.with? "glpk"
+  depends_on "homebrew/science/asl" => :optional
   depends_on "homebrew/science/mumps" => [:optional, "without-mpi"] + openblas_dep
   depends_on "homebrew/science/suite-sparse" => [:optional] + openblas_dep
+  depends_on "readline" => :recommended
 
   mumps_dep = (build.with? "mumps") ? ["with-mumps"] : []
   suite_sparse_dep = (build.with? "suite-sparse") ? ["with-suite-sparse"] : []
@@ -24,7 +31,7 @@ class Symphony < Formula
   depends_on "mysql" => :build if build.with? "gmpl"
   depends_on "readline" => :recommended
 
-  depends_on "clp" => (asl_dep + openblas_dep + mumps_dep + suite_sparse_dep)
+  depends_on "clp" => (asl_dep + glpk_dep + mumps_dep + openblas_dep + suitesparse_dep)
   depends_on "cgl"
   depends_on :fortran
 
@@ -46,12 +53,9 @@ class Symphony < Formula
       ENV.append "LDFLAGS",  "-L#{Formula["readline"].opt_lib} -lreadline"
     end
 
-    if build.with? "gmpl"
-      # Symphony uses a patched version of GLPK for reading MPL files.
-      # Use a private version rather than require the Homebrew version of GLPK.
-      cd "ThirdParty/Glpk" do
-        system "./get.Glpk"
-      end
+    if build.with? "glpk"
+      args << "--with-glpk-lib=-L#{Formula["glpk448"].opt_lib} -lglpk"
+      args << "--with-glpk-incdir=#{Formula["glpk448"].opt_include}"
       ENV.append "CPPFLAGS", "-I#{Formula["mysql"].opt_include}/mysql"
       args << "--with-gmpl"
     end
