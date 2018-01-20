878c65591eca10c4e93963d8b18c61c5de4ddef5
Dominique Orban <dominique.orban@gmail.com>
2018-01-20 15:25:13 +0100

    dylp 1.10.3

--- dylp.rb
+++ dylp.rb
@@ -1,8 +1,8 @@
 class Dylp < Formula
   desc "Dynamic simplex algorithm for linear programming"
   homepage "https://projects.coin-or.org/DyLP"
-  url "https://www.coin-or.org/download/pkgsource/DyLP/DyLP-1.10.2.tgz"
-  sha256 "d17ff86c313aec7e6e00aa7abb3665446390096570cd141bf438afd65723db90"
+  url "https://www.coin-or.org/download/pkgsource/DyLP/DyLP-1.10.3.tgz"
+  sha256 "34984099975e324864ec8b585208007708097ca9d1738e9a3ee6292f43942456"
   head "https://projects.coin-or.org/svn/DyLP/trunk"
 
   depends_on "osi"
