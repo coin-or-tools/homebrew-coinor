ea32ffbac9638c84bbbc1d36bb1b00ffadfda5a1
Shane Burke <68519104+DrShaneBurke@users.noreply.github.com>
2021-03-06 02:36:40 -0500

    Update coin_data_miplib3.rb

--- coin_data_miplib3.rb
+++ coin_data_miplib3.rb
@@ -1,8 +1,8 @@
 class CoinDataMiplib3 < Formula
   desc "MIPLib models"
   homepage "https://www.coin-or.org/download/source/Data"
-  url "https://www.coin-or.org/download/source/Data/Data-miplib3-1.2.7.tgz"
-  sha256 "7a6b15435e77e6758d605d0654f0fd2aeeec2d34061b0e95e71f698a047cbb4b"
+  url "https://www.coin-or.org/download/source/Data/Data-miplib3-1.2.8.tgz"
+  sha256 ""
 
   def install
     system "./configure", "--disable-debug",
