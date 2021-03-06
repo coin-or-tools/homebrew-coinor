7b37ed52d041ebad93455ccd584a00497c4e020b
Shane Burke <68519104+DrShaneBurke@users.noreply.github.com>
2021-03-06 02:48:55 -0500

    Updated url to most up-to-date version
    1. Updated version reference to most recent (1.2.8)
    2. Updated sha256 for appropriate version (pending)

--- coin_data_miplib3.rb
+++ coin_data_miplib3.rb
@@ -2,7 +2,7 @@ class CoinDataMiplib3 < Formula
   desc "MIPLib models"
   homepage "https://www.coin-or.org/download/source/Data"
   url "https://www.coin-or.org/download/source/Data/Data-miplib3-1.2.8.tgz"
-  sha256 " "
+  sha256 ""
 
   def install
     system "./configure", "--disable-debug",
