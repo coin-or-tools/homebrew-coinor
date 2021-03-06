5a2bad8c84ad320efaa34b61b250ac344e004626
Shane Burke <68519104+DrShaneBurke@users.noreply.github.com>
2021-03-06 02:42:55 -0500

    Updated url to most up-to-date version
    1. Updated version reference to most recent (1.2.12)
    2. Updated sha256 for appropriate version (pending)

--- coin_data_sample.rb
+++ coin_data_sample.rb
@@ -1,8 +1,8 @@
 class CoinDataSample < Formula
   desc "Sample models"
   homepage "https://www.coin-or.org/download/source/Data"
-  url "https://www.coin-or.org/download/source/Data/Data-Sample-1.2.11.tgz"
-  sha256 "7d201dc37098dd1f7d68c24d71ca8083eaaa344ec44bd18799ac6245363f8467"
+  url "https://www.coin-or.org/download/source/Data/Data-Sample-1.2.12.tgz"
+  sha256 ""
 
   def install
     system "./configure", "--disable-debug",
