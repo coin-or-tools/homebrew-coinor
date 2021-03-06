ede59a1d69e937a9203cedffd00e34ab6de682c8
Shane Burke <68519104+DrShaneBurke@users.noreply.github.com>
2021-03-06 02:47:36 -0500

    Updated url to most up-to-date version
    1. Updated version reference to most recent (1.1.7)
    2. Updated sha256 for appropriate version (pending)

--- coin_data_stochastic.rb
+++ coin_data_stochastic.rb
@@ -1,8 +1,8 @@
 class CoinDataStochastic < Formula
   desc "Stochastic models"
   homepage "https://www.coin-or.org/download/source/Data"
-  url "https://www.coin-or.org/download/source/Data/Data-Stochastic-1.1.6.tgz"
-  sha256 "2e2df9cd16bb8d07739cc5b14f0b52a6e697cd181e5e3c793cff83804491c255"
+  url "https://www.coin-or.org/download/source/Data/Data-Stochastic-1.1.7.tgz"
+  sha256 ""
 
   def install
     system "./configure", "--disable-debug",
