4c1ea0bab13e0272d38e2f32ce5ad41f6751d27d
Shane Burke <68519104+DrShaneBurke@users.noreply.github.com>
2021-03-06 02:41:08 -0500

    Updated url to most up-to-date version
    1. Updated version reference to most recent (1.2.9)
    2. Updated sha256 for appropriate version (pending)

--- coin_data_netlib.rb
+++ coin_data_netlib.rb
@@ -1,8 +1,8 @@
 class CoinDataNetlib < Formula
   desc "Netlib LP models"
   homepage "https://www.coin-or.org/download/source/Data"
-  url "https://www.coin-or.org/download/source/Data/Data-Netlib-1.2.8.tgz"
-  sha256 "f6358d756a1d0ae2ac159a422ceb661f080bf527f7b5a3e30320839beb8de3e4"
+  url "https://www.coin-or.org/download/source/Data/Data-Netlib-1.2.9.tgz"
+  sha256 ""
 
   def install
     system "./configure", "--disable-debug",
