7782aad9659e7bdd5fa56012a76fb399405b1d37
Alexander-Belyi <78016743+Alexander-Belyi@users.noreply.github.com>
2022-01-20 11:24:23 +0100

    netlib: update to version 1.2.10

--- coin_data_netlib.rb
+++ coin_data_netlib.rb
@@ -1,9 +1,8 @@
 class CoinDataNetlib < Formula
   desc "Netlib LP models"
   homepage "https://github.com/coin-or-tools/Data-Netlib"
-  url "https://github.com/coin-or-tools/Data-Netlib/archive/refs/tags/releases/1.2.8.tar.gz"
-  sha256 "4675357aae83d70ebf1b5874bbda1bd89fabf462dac7e4a0fffb202b1e9c4129"
-  revision 1
+  url "https://github.com/coin-or-tools/Data-Netlib/archive/refs/tags/releases/1.2.10.tar.gz"
+  sha256 "a1526411fe0bd9e83e1f9df5b68f17f45e7138b0327c7786d3cc2b578a738074"
 
   head "https://github.com/coin-or-tools/Data-Netlib.git"
 
