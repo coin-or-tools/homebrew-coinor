a895258df4dc7ac1db19e1369fceb6a8348ad0e8
Alexander-Belyi <78016743+Alexander-Belyi@users.noreply.github.com>
2022-01-20 11:15:30 +0100

    coin_data_miplib3 1.2.8
    miplib3: update to version 1.2.8

--- coin_data_miplib3.rb
+++ coin_data_miplib3.rb
@@ -1,9 +1,8 @@
 class CoinDataMiplib3 < Formula
   desc "MIPLib models"
   homepage "https://github.com/coin-or-tools/Data-miplib3"
-  url "https://github.com/coin-or-tools/Data-miplib3/archive/refs/tags/releases/1.2.7.tar.gz"
-  sha256 "49578f714cd87247510747b8c63886ed3a6a1a90ffbbbf7e3cb772657737d622"
-  revision 1
+  url "https://github.com/coin-or-tools/Data-miplib3/archive/refs/tags/releases/1.2.8.tar.gz"
+  sha256 "f7899e59bff4c597d91a24c7462d473358cd37b2877dc040eccd390b56933f9c"
 
   head "https://github.com/coin-or-tools/Data-miplib3.git"
 
