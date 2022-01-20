5717584cbfa7ebfa85c3d9bfd0af7e730825282d
Alexander-Belyi <78016743+Alexander-Belyi@users.noreply.github.com>
2022-01-20 11:35:32 +0100

    coin_data_stochastic 1.1.7
    data-stochastic: update to version 1.1.7
    
    Closes #74.
    
    Signed-off-by: Dominique <dominique.orban@gmail.com>

--- coin_data_stochastic.rb
+++ coin_data_stochastic.rb
@@ -1,9 +1,8 @@
 class CoinDataStochastic < Formula
   desc "Stochastic models"
   homepage "https://github.com/coin-or-tools/Data-Stochastic"
-  url "https://github.com/coin-or-tools/Data-Stochastic/archive/refs/tags/releases/1.1.6.tar.gz"
-  sha256 "0ce99bfd3ae2b42f858fac2ca4d77b6ef89d2b2f215fd63cd61ead51e9988f1f"
-  revision 1
+  url "https://github.com/coin-or-tools/Data-Stochastic/archive/refs/tags/releases/1.1.7.tar.gz"
+  sha256 "de65ca248c23dcdfc13d0212f903de034f48c9d7c22ee249ce65afc8372fd9cb"
 
   head "https://github.com/coin-or-tools/Data-Stochastic.git"
 
