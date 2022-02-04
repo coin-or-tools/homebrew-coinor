37b0bea96c02bc19cfbd8ff2a03372682ed133b3
Ted Ralphs <ted@lehigh.edu>
2022-02-04 14:23:24 +0000

    coin_data_miplib3: add 1.2.7_1 bottle.

--- coin_data_miplib3.rb
+++ coin_data_miplib3.rb
@@ -7,6 +7,13 @@ class CoinDataMiplib3 < Formula
 
   head "https://github.com/coin-or-tools/Data-miplib3.git"
 
+  bottle do
+    root_url "https://github.com/coin-or-tools/homebrew-coinor/releases/download/coin_data_miplib3-1.2.7_1"
+    sha256 cellar: :any_skip_relocation, big_sur:      "9685f3412bfbae39080ad00804b1167e5ba5154059145604ff13d7a4fa41bc22"
+    sha256 cellar: :any_skip_relocation, catalina:     "f238365016b7ce5de35f985461c060c9fd631b1b1d97f59c10e3da57f5b82237"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "cd8931f9109c8ebcd0cba2e790f123cfe06679c26cd6138072682379803a5021"
+  end
+
   def install
     system "./configure", "--disable-debug",
            "--disable-dependency-tracking",
