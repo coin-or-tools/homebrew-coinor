497db79faf0b72255fa4577b140c7ab8df7e76eb
Ted Ralphs <ted@lehigh.edu>
2022-02-04 14:23:25 +0000

    dylp: add 1.10.4_1 bottle.

--- dylp.rb
+++ dylp.rb
@@ -7,6 +7,13 @@ class Dylp < Formula
 
   head "https://github.com/coin-or/DyLP.git"
 
+  bottle do
+    root_url "https://github.com/coin-or-tools/homebrew-coinor/releases/download/dylp-1.10.4_1"
+    sha256 cellar: :any,                 big_sur:      "60ae7cae035dd7bcb4f6569ba4875cbdc456ab408938f469d484150ed5d11686"
+    sha256 cellar: :any,                 catalina:     "fdc3f16f2765661ed2be22b1a18cfb59f66f6705483d801470e7e8c4f532f5e3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "a74f36a134e76e42db9381a9a7403878d6788222f2372cfb60c76e8f4e151c26"
+  end
+
   depends_on "coin-or-tools/coinor/osi"
 
   def install
