97ed3cbb41a2a65d71b8195beb2901cfb75c8117
Ted Ralphs <ted@lehigh.edu>
2022-02-23 23:27:59 -0500

    New version of Cgl (#78)

--- cgl.rb
+++ cgl.rb
@@ -1,9 +1,8 @@
 class Cgl < Formula
   desc "Cut-generation library"
   homepage "https://github.com/coin-or/Cgl"
-  url "https://github.com/coin-or/Cgl/archive/refs/tags/releases/0.60.4.tar.gz"
-  sha256 "57db498a0b7e1f3614ca061a93b23dc7e65017f092457c7366fa7d78397b5657"
-  revision 1
+  url "https://github.com/coin-or/Cgl/archive/refs/tags/releases/0.60.5.tar.gz"
+  sha256 "5a2e7ca380425b3d7279d0759c625a367d06ec8293698b59f82fae38ae5df64e"
 
   head "https://github.com/coin-or/Cgl.git"
 
@@ -29,7 +28,9 @@ class Cgl < Formula
             "--with-sample-datadir=#{Formula["coin-or-tools/coinor/coin_data_sample"].opt_pkgshare}/coin/Data/Sample",
             "--with-clp-lib=-L#{Formula["coin-or-tools/coinor/clp"].opt_lib} -lClp",
             "--with-clp-incdir=#{Formula["coin-or-tools/coinor/clp"].opt_include}/clp/coin",
-            "--with-dot"]
+            "--with-dot",
+            "--without-osidylp",
+            "--without-osivol"]
 
     system "./configure", *args
 
