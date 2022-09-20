d85bade42a43b81ad9bc5d2837b6fa7ba73a478a
Halil Sen <halilsen@gmail.com>
2022-09-20 15:27:58 +0200

    Fix: MUMPS source is regularly down use the mirror
    per [this comment](https://github.com/coin-or/Cbc/issues/529#issuecomment-1200666236)

--- mumps-seq.rb
+++ mumps-seq.rb
@@ -1,7 +1,7 @@
 class MumpsSeq < Formula
   desc "Sequential Sparse Direct Solver"
   homepage "http://mumps-solver.org"
-  url "http://mumps.enseeiht.fr/MUMPS_5.4.1.tar.gz"
+  url "http://coin-or-tools.github.io/ThirdParty-Mumps/MUMPS_5.4.1.tar.gz"
   sha256 "93034a1a9fe0876307136dcde7e98e9086e199de76f1c47da822e7d4de987fa8"
 
   bottle do
