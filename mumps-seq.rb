509b4d35f0c693552362db3bae7ee372426a2ecf
Dominique <dominique.orban@gmail.com>
2022-09-21 23:16:56 -0400

    Update mumps-seq.rb

--- mumps-seq.rb
+++ mumps-seq.rb
@@ -4,6 +4,7 @@ class MumpsSeq < Formula
   url "http://mumps.enseeiht.fr/MUMPS_5.4.1.tar.gz"
   mirror "https://graal.ens-lyon.fr/MUMPS/MUMPS_5.4.1.tar.gz"
   sha256 "93034a1a9fe0876307136dcde7e98e9086e199de76f1c47da822e7d4de987fa8"
+  revision 1
 
   bottle do
     root_url "https://github.com/coin-or-tools/homebrew-coinor/releases/download/mumps-seq-5.4.1"
