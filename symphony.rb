0425be0e7289cc5d8ff0d8dc498b7818ccd8d8a0
Ted Ralphs <ted@lehigh.edu>
2023-04-19 16:15:09 -0400

    Update symphony.rb

--- symphony.rb
+++ symphony.rb
@@ -1,8 +1,8 @@
 class Symphony < Formula
   desc "Framework for solving mixed integer linear programs"
   homepage "https://github.com/coin-or/SYMPHONY"
-  url "https://github.com/coin-or/SYMPHONY/archive/refs/tags/releases/5.6.18.tar.gz"
-  sha256 "f566e2986c6b4269a5a128cea13622d3d90b046b7a9151ebd89f27c495f183a0"
+  url "https://github.com/coin-or/SYMPHONY/archive/refs/tags/releases/5.7.0.tar.gz"
+  sha256 "090616c2fc43ab83b6e980550a5b3f5dd87143fa9583c327c3e6712483eea18c"
 
   head "https://github.com/coin-or/SYMPHONY"
 
