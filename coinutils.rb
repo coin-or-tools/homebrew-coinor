b56b85f3c714cd3f0869007af10d171ba668c142
Ted Ralphs <ted@lehigh.edu>
2023-04-19 12:52:52 -0400

    Update CoinUtils version

--- coinutils.rb
+++ coinutils.rb
@@ -2,7 +2,7 @@ class Coinutils < Formula
   desc "Utilities used by other Coin-OR projects"
   homepage "https://github.com/coin-or/CoinUtils"
   url "https://github.com/coin-or/CoinUtils/archive/refs/tags/releases/2.11.6.tar.gz"
-  sha256 "6ea31d5214f7eb27fa3ffb2bdad7ec96499dd2aaaeb4a7d0abd90ef852fc79ca"
+  sha256 "202e347d1c1d2ccf5355e3c2874a4dc16500226c180b00d6677f464d80be337e"
   revision 1
 
   head "https://github.com/coin-or/CoinUtils.git"
