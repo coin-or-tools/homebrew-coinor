cbe398c8c1ba995d509f8b5af24ca9a1a4b0c20c
Ted Ralphs <ted@lehigh.edu>
2024-08-09 15:34:07 +0200

    Remove SYMPHONY dependence on DyLP (#98)
    * Update symphony.rb
    
    * Remove outdated
    
    * Update revision number
    
    * Remove dylp configure argument

--- symphony.rb
+++ symphony.rb
@@ -3,6 +3,7 @@ class Symphony < Formula
   homepage "https://github.com/coin-or/SYMPHONY"
   url "https://github.com/coin-or/SYMPHONY/archive/refs/tags/releases/5.6.18.tar.gz"
   sha256 "f566e2986c6b4269a5a128cea13622d3d90b046b7a9151ebd89f27c495f183a0"
+  revision 1
 
   head "https://github.com/coin-or/SYMPHONY"
 
@@ -17,7 +18,6 @@ class Symphony < Formula
 
   depends_on "coin-or-tools/coinor/cgl"
   depends_on "coin-or-tools/coinor/clp"
-  depends_on "coin-or-tools/coinor/dylp"
   depends_on "coin-or-tools/coinor/glpk@448"
   depends_on "gcc"
   depends_on "readline"
@@ -42,9 +42,6 @@ class Symphony < Formula
 
     args << "--with-clp-lib=-L#{Formula["coin-or-tools/coinor/clp"].opt_lib} -lClp -lOsiClp"
     args << "--with-clp-incdir=#{Formula["coin-or-tools/coinor/clp"].opt_include}/clp/coin"
-
-    args << "--with-dylp-lib=-L#{Formula["coin-or-tools/coinor/dylp"].opt_lib} -lDylp"
-    args << "--with-dylp-incdir=#{Formula["coin-or-tools/coinor/dylp"].opt_include}/dylp/coin"
 
     args << "--with-glpk-lib=-L#{Formula["coin-or-tools/coinor/glpk@448"].opt_lib} -lglpk"
     args << "--with-glpk-incdir=#{Formula["coin-or-tools/coinor/glpk@448"].opt_include}"
