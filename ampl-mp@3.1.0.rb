6e340561dbc68f2abfbf0f99476fcfc0f6a3336c
Ted Ralphs <ted@lehigh.edu>
2025-02-03 09:07:48 -0500

    Backporting some stuff that used to be in the recipe when the version was 3.1.0 https://github.com/Homebrew/homebrew-core/commit/30894b2945f397368a1e6d1eb9c77a3352cf7a97

--- ampl-mp@3.1.0.rb
+++ ampl-mp@3.1.0.rb
@@ -11,9 +11,32 @@ class AmplMpAT310 < Formula
   end
 
   depends_on "cmake" => :build
-  depends_on "gcc@9"
 
-  patch :DATA
+  resource "miniampl" do
+    url "https://github.com/dpo/miniampl/archive/refs/tags/v1.0.tar.gz"
+    sha256 "b836dbf1208426f4bd93d6d79d632c6f5619054279ac33453825e036a915c675"
+  end
+
+  # Removes Google Benchmark - as already done so upstream
+  # All it did was conflict with the google-benchmark formula
+  patch do
+    url "https://github.com/ampl/mp/commit/96e332bb8cb7ba925e3ac947d6df515496027eed.patch?full_index=1"
+    sha256 "1a4ef4cd1f4e8b959c20518f8f00994ef577e74e05824b2d1b241b1c3c1f84eb"
+  end
+
+  # Install missing header files, remove in > 3.1.0
+  # https://github.com/ampl/mp/issues/110
+  patch do
+    url "https://github.com/ampl/mp/commit/8183be3e486d38d281e0c5a02a1ea4239695035e.patch?full_index=1"
+    sha256 "6b37201f1d0d6dba591e7e1b81fb16d2694d118605c92c422dcdaaedb463c367"
+  end
+  # Backport fmt header update. Remove in the next release
+  # https://github.com/ampl/mp/issues/108
+
+  patch do
+    url "https://github.com/ampl/mp/commit/1f39801af085656e4bf72250356a3a70d5d98e73.patch?full_index=1"
+    sha256 "b0e0185f24caba54cb38b65a638ebda6eb4db3e8c74d71ca79f072b8337e8e2c"
+  end
 
   def install
     args = %W[
@@ -29,53 +52,12 @@ class AmplMpAT310 < Formula
   end
 
   test do
-    (testpath/"test.c").write <<~C
-      #include <stdio.h>
-      #include <stdlib.h>
-      #include "mp/ampls-c-api.h"
-
-      int main() {
-          AMPLS_MP_Solver* solver = (AMPLS_MP_Solver*)malloc(sizeof(AMPLS_MP_Solver));
-          free(solver);
-          return 0;
-      }
-    C
-
-    system ENV.cc, "test.c", "-I#{include}/mp", "-L#{lib}", "-lmp", "-o", "test"
-    system "./test"
+    resource("miniampl").stage do
+      testpath.install "src/miniampl.c", Dir["examples/wb.*"]
+    end
+    system ENV.cc, "miniampl.c", "-std=c99", "-I#{include}/asl", "-L#{lib}", "-lasl", "-lmp"
+    output = shell_output("./a.out wb showname=1 showgrad=1")
+    assert_match "Objective name: objective", output
   end
 end
 
-__END__
---- a/include/mp/format.h
-+++ b/include/mp/format.h
-@@ -1747,21 +1747,21 @@
-     typedef typename BasicWriter<Char>::CharPtr CharPtr;
-     Char fill = internal::CharTraits<Char>::cast(spec_.fill());
-     CharPtr out = CharPtr();
--    const unsigned CHAR_WIDTH = 1;
--    if (spec_.width_ > CHAR_WIDTH) {
-+    const unsigned C_WIDTH = 1;
-+    if (spec_.width_ > C_WIDTH) {
-       out = writer_.grow_buffer(spec_.width_);
-       if (spec_.align_ == ALIGN_RIGHT) {
--        std::uninitialized_fill_n(out, spec_.width_ - CHAR_WIDTH, fill);
--        out += spec_.width_ - CHAR_WIDTH;
-+        std::uninitialized_fill_n(out, spec_.width_ - C_WIDTH, fill);
-+        out += spec_.width_ - C_WIDTH;
-       } else if (spec_.align_ == ALIGN_CENTER) {
-         out = writer_.fill_padding(out, spec_.width_,
--                                   internal::check(CHAR_WIDTH), fill);
-+                                   internal::check(C_WIDTH), fill);
-       } else {
--        std::uninitialized_fill_n(out + CHAR_WIDTH,
--                                  spec_.width_ - CHAR_WIDTH, fill);
-+        std::uninitialized_fill_n(out + C_WIDTH,
-+                                  spec_.width_ - C_WIDTH, fill);
-       }
-     } else {
--      out = writer_.grow_buffer(CHAR_WIDTH);
-+      out = writer_.grow_buffer(C_WIDTH);
-     }
-     *out = internal::CharTraits<Char>::cast(value);
-   }
