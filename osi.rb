f9e141e1d69dd226b56ea7a5437668672f4eddf2
Ted Ralphs <ted@lehigh.edu>
2026-03-12 22:03:52 -0400

    Style

--- osi.rb
+++ osi.rb
@@ -16,8 +16,8 @@ class Osi < Formula
   depends_on "pkg-config" => :build
 
   depends_on "coin-or-tools/coinor/coinutils"
-  depends_on "coin-or-tools/coinor/glpk@448"
   depends_on "bzip2"
+  depends_on "coin-or-tools/coinor/glpk@448"
   depends_on "zlib-ng-compat"
   depends_on "openblas" => :recommended
 
