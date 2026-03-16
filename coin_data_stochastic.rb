57dc3cfd3a3cb578ee28f9fb04ad305a6a9f45b4
Ted Ralphs <ted@lehigh.edu>
2026-03-16 18:21:50 +0000

    coin_data_stochastic: update 1.1.7_1 bottle.

--- coin_data_stochastic.rb
+++ coin_data_stochastic.rb
@@ -8,10 +8,11 @@ class CoinDataStochastic < Formula
   head "https://github.com/coin-or-tools/Data-Stochastic.git"
 
   bottle do
-    root_url "https://github.com/coin-or-tools/homebrew-coinor/releases/download/coin_data_stochastic-1.1.7"
-    sha256 cellar: :any_skip_relocation, big_sur:      "d93167bfb7bdc4cfa4b3ac45cf5b4332b46810cbe083d3541c1b772b5ca25c38"
-    sha256 cellar: :any_skip_relocation, catalina:     "d0d1af97f5f8b6d71f2a766d899314cbe7362246863c66a45ad9f1fb78f8145d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "bd75c5531f8f5c6b325a0ef9799adbc6868dd4f29251eedc5a8251c74ff47a39"
+    root_url "https://github.com/coin-or-tools/homebrew-coinor/releases/download/coin_data_stochastic-1.1.7_1"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "44a8072fd02d0bbed8b995bf0fc1ab053c970d593048892e15759b6711a807e4"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "aa3224e3cbd12cb616781cebf8bc5bd9d2f3c0d37ce6a4194018c059b26ac44d"
+    sha256 cellar: :any_skip_relocation, sequoia:      "3ba3c0b6dc89f91383f400fc922d98ab0c1dfd56194d9dececf9a2a9831d38bc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "af8217e8cb24048608b032db2d7e62f8e55010f7bd3768e8dd96fc5a75de8409"
   end
 
   def install
