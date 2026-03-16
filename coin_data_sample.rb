5b62f39037915294c1773f8840bbb5c749f563f3
Ted Ralphs <ted@lehigh.edu>
2026-03-16 18:21:50 +0000

    coin_data_sample: update 1.2.13 bottle.

--- coin_data_sample.rb
+++ coin_data_sample.rb
@@ -7,10 +7,11 @@ class CoinDataSample < Formula
   head "https://github.com/coin-or-tools/Data-Sample.git"
 
   bottle do
-    root_url "https://github.com/coin-or-tools/homebrew-coinor/releases/download/coin_data_sample-1.2.12"
-    sha256 cellar: :any_skip_relocation, big_sur:      "26c9b55072710e84714f0af050cec53e863f3b8cfed67ed2233b2630c8998fab"
-    sha256 cellar: :any_skip_relocation, catalina:     "eef9ed62cea280a7ae01ad6439f262e06295223239d040a5e3a99f453f9b51a4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "bdda762446f81a97fd5cac3b48d234cfbaddc510560ea784335ef0d626fe1b73"
+    root_url "https://github.com/coin-or-tools/homebrew-coinor/releases/download/coin_data_sample-1.2.13"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "e64d636fac75b6e38ce9a9df5421d42a665e12efdcabc98665ef59dbd1ed5af3"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "f55aae183e32b010721db4dd7b1a527666a62648d0e75e92039553d1d59afb99"
+    sha256 cellar: :any_skip_relocation, sequoia:      "409b329530f382767efa1982ba71e3b03b30efca6a79d59f7e0a273088530ec4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "7a62928fa2833b091a70f1dbf80e09a7f744bd25a8d0c2e4a0621a3b95e470ee"
   end
 
   def install
